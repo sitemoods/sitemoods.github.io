43f2e3f252f3b093e2859d60ec52d497b9c91a8b
Fred <nagual@nagual.local>
2023-10-20 14:11:42 -0300

    auto

--- script.js
+++ script.js
@@ -4,21 +4,31 @@ function openModal(event) {
 
     modalcontent.style.display = "block";
     document.body.style.overflow = "hidden";
+    // Find and play the video inside the modal
+    var video = modalcontent.querySelector("video");
+    if (video) {
+        video.play();
+    }
 
     // Define a function to close the modal when the content is clicked
     function closeModalOnClick() {
         modalcontent.style.display = "none";
         document.body.style.overflow = "auto";
+        document.body.style.overflowX = "hidden";
 
         // Remove the event listener after use
         modalcontent.removeEventListener("click", closeModalOnClick);
+        
     }
 
     // Add an event listener to the modal content to close it
     modalcontent.addEventListener("click", closeModalOnClick);
+
+
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    document.body.style.overflowX = "hidden";
     var modalClickables = document.querySelectorAll(".modal-clickable");
     modalClickables.forEach(function (element) {
         element.addEventListener("click", openModal);
